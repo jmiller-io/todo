7974206bd4434ccb31a9bcc90dc90d3a8e2297cc
Jake Miller <hillsighed@gmail.com>
2017-05-23 13:24:42 -0600

    creates VC for all adventures

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,8 +1,26 @@
 class AdventuresController < ApplicationController
+
+  def index
+    @adventures = Adventure.all
+  end
+
+  def show
+    @adventure = Adventure.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:adventure].inspect
+    @adventure = Adventure.new(adventure_params)
+
+    @adventure.save
+    redirect_to @adventure
   end
+
+  private
+    def adventure_params
+      params.require(:adventure).permit(:what, :where, :when, :with)
+    end
+
 end
