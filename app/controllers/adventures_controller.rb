c507e8a230053efa1ef1ce47e5052fc64273664e
Jake Miller <hillsighed@gmail.com>
2017-05-23 14:08:48 -0600

    adds destroy action

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -12,6 +12,10 @@ class AdventuresController < ApplicationController
     @adventure = Adventure.new
   end
 
+  def edit
+    @adventure = Adventure.find(params[:id])
+  end
+
   def create
     @adventure = Adventure.new(adventure_params)
 
@@ -20,6 +24,22 @@ class AdventuresController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @adventure = Adventure.find(params[:id])
+
+    if @adventure.update(adventure_params)
+      redirect_to @adventure
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @adventure = Adventure.find(params[:id])
+    @adventure.destroy
+    redirect_to adventures_path
   end
 
   private
